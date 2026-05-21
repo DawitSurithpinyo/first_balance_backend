1ca6a93bb435181d97dceabd5fc10e47befb305c
Dawit <dawitsurithpinyo@gmail.com>
2026-05-21 16:09:08 +0700

    feat(auth): add client_id of Google OAuth in config

--- config/googleOAuthConfig.py
+++ config/googleOAuthConfig.py
@@ -3,7 +3,8 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DEV_CLIENT_SECRETS_FILE = os.getenv('DEV_CLIENT_SECRETS_FILE', 'no file')
-SCOPES = ['https://www.googleapis.com/auth/userinfo.email', 
-          'https://www.googleapis.com/auth/userinfo.profile',
-          'openid']
+DEV_CLIENT_SECRETS_FILE = os.getenv('DEV_CLIENT_SECRETS_FILE')
+DEV_CLIENT_ID = os.getenv('DEV_CLIENT_ID')
+SCOPES = ['https://www.googleapis.com/auth/userinfo.profile', 
+          'https://www.googleapis.com/auth/userinfo.email',
+        'openid']
